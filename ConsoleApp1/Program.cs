d2d2bf74547a1aac9cff6c1f9ff84d0dc0ffcb7a
Koksal Jan <JaniK@code.local>
2022-12-07 21:13:28 +0400

    csharp

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿int deger = 51;
+if (deger <= 100 && deger >= 91)
+{
+    Console.WriteLine("A");
+}
+else if (deger <= 90 && deger >= 81)
+{
+
+    Console.WriteLine("B");
+}
+else if (deger <= 80 && deger >= 71)
+{
+    Console.WriteLine("C");
+}
+else if (deger <= 70 && deger >= 61)
+{
+
+    Console.WriteLine("D");
+}
+else if (deger <= 60 && deger >= 51)
+{
+    Console.WriteLine("E");
+}
+else if (deger <=50)
+{
+    Console.WriteLine("kaldiniz");
+}
+ 
+
+ 
+
+
+
